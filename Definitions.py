a50615abed4ab32600174b2744e5348f7f174002
PrabhaasNakkala <58062567+PrabhaasNakkala@users.noreply.github.com>
2023-07-17 22:15:27 -0700

    Delete Definitions.py

--- Definitions.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def Definitions(word: str):
-    text = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en/' + word)
-    definition = text.text[text.text.find("\"definition\":") + 14:text.text.find("\"", text.text.find("\"definition\":") + 14)]
-    if definition == "Definitions Found":
-        return '$Error$'
-    return definition
